011b84fde41868193488d61f655bdc09ca939982
Student <student@Makerss-MacBook-Air-5.local>
2020-03-03 13:55:58 +0000

    Some Little edits on the files name

---
+++ note-model.js
@@ -0,0 +1,13 @@
+// (function(viewNote){
+//   function Note(){
+//     this.note =note;
+//   }
+//   viewNote.Note = Note;
+
+// })(this);
+//CLASS NOTEPAD
+
+function Note (_name, _size){
+  this._name = _name ||"unknown";
+  this._size = _size || "20"
+}
